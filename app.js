6841a1ac088a7014463f8f8a9e477e72ebec107f
Navilongo <navilongo@gmail.com>
2021-05-22 11:50:49 -0700

    completed drop down menu

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // Use D3 library to read the samples.json
-d3.json("samples.json").then((data) => {
-    console.log(data);
+d3.json("samples.json").then(data => {
+    var imported = data;
+    console.log(imported);
+
+    var nameSubject = data.names;
+    var dropDown = d3.select("#selDataset")
+
+    nameSubject.forEach(name => {
+        dropDown.append("option").text(name).property("value", name);
 });
-// 
+
+    
+    
+// testing bar charts
+/*
 var data = [{
     type: 'bar',
     x: [20, 14, 23],
@@ -10,6 +21,11 @@ var data = [{
     orientation: 'h'
     }];
  Plotly.newPlot('bar', data);
+
+ */
+
+
+ 
 // Create horizontal bar with a dropdown menu to display the top 10 OTUs found in that individual.
 
 // Use sample_values as the values for the bar chart
